f6e94c6607e23ed5bb11753d1969624cbc38e123
aaargha <aaargha@users.noreply.github.com>
2017-12-13 19:55:52 +0100

    Upload of version 0.1.2

---
+++ prototypes/hotkeys.lua
@@ -0,0 +1,12 @@
+data:extend({
+  {
+    type = "custom-input",
+    name = "infinizoom_increase_zoom",
+    key_sequence = "CONTROL + K",
+  },
+  {
+    type = "custom-input",
+    name = "infinizoom_decrease_zoom",
+    key_sequence = "CONTROL + L",
+  }
+})
