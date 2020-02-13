d5b4add69ebd382e7625bbdb3bd27cbde0b7996a
calcwizard <49937136+calcwizard@users.noreply.github.com>
2020-02-13 01:51:32 -0500

    Updated hotkeys to piggyback vanilla
    Changed the hotkeys to piggyback and consume the vanilla zoom controls (scroll wheel up and down)

--- prototypes/hotkeys.lua
+++ prototypes/hotkeys.lua
@@ -2,11 +2,15 @@ data:extend({
   {
     type = "custom-input",
     name = "infinizoom_increase_zoom",
-    key_sequence = "CONTROL + K",
+    key_sequence = "",
+    linked_game_control = "zoom-in",
+    consuming = "game-only",
   },
   {
     type = "custom-input",
     name = "infinizoom_decrease_zoom",
-    key_sequence = "CONTROL + L",
+    key_sequence = "",
+    linked_game_control = "zoom-out",
+    consuming = "game-only",
   }
 })
