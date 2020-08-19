f4b3f865be08bdad23b77949f2391261f968f218
aaargha <aaargha@users.noreply.github.com>
2020-08-19 21:57:41 +0200

    Reverted back to dedicated hotkeys for zooming.

--- prototypes/hotkeys.lua
+++ prototypes/hotkeys.lua
@@ -2,16 +2,12 @@ data:extend({
   {
     type = "custom-input",
     name = "infinizoom_increase_zoom",
-    key_sequence = "",
-    linked_game_control = "zoom-in",
-    consuming = "none"
+    key_sequence = "CONTROL + K"
   },
   {
     type = "custom-input",
     name = "infinizoom_decrease_zoom",
-    key_sequence = "",
-    linked_game_control = "zoom-out",
-    consuming = "none"
+    key_sequence = "CONTROL + L"
   },
   {
     type = "custom-input",
